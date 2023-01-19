cde919488609810dfaeada9a0f242f4d2d82c03b
Yaroslav <pakhmutovyaroslav@gmail.com>
2023-01-19 22:56:51 +0300

    lw1 init

---
+++ lw1/ConversionMealyMoore/ConversionMealyMoore/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConversionMealyMoore.Extensions;
+using ConversionMealyMoore.Handlers;
+using ConversionMealyMoore.Types;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConversionMealyMoore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> filesNames = args.GetFilesNames();
+            ConversionType conversionType = args.GetConversionType();
+
+            using StreamReader inFile = new(filesNames[0]);
+            using StreamWriter outFile = new(filesNames[1]);
+
+            FileHandler fileHandler = new(inFile, outFile);
+
+
+        }
+    }
+}
